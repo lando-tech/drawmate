6329519ae0618c3f27ed40c8a893c92b2b9704ff
aaron newman <aaronlnewman@gmail.com>
2025-08-02 15:48:21 +0900

    Add AbstractLayoutManager class definition with base_x and base_y members

---
+++ engine/include/abstract_layout_manager.h
@@ -0,0 +1,16 @@
+#ifndef ABSTRACT_LAYOUT_MANAGER_H
+#define ABSTRACT_LAYOUT_MANAGER_H
+
+class AbstractLayoutManager {
+
+private:
+
+    double base_x{};
+    double base_y{};
+
+public:
+
+
+};
+
+#endif
