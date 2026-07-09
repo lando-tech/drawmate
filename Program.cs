3f7b2333a735a28ba7940a092df8e0619b0d08aa
aaron newman <aaron@landotech.io>
2026-07-09 14:54:10 -0500

    Remove test_templates directory and associated JSON configuration files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DrawmateLib.DocumentContexts;
+using DrawmateLib.Builders;
+
+var drawmateContext = new DrawmateContext();
+var mxCellBuilder = new MxCellBuilder();
+var mxStyleBuilder = new MxStyleBuilder();
+
+var mxCellOne = mxCellBuilder
+    .Create(
+        "Cell 1", 
+        mxStyleBuilder
+            .Create().Rectangle().Build(true).Value)
+    .WithGeometry(400.0m, 400.0m, 50, 50)
+    .Build();
+
+drawmateContext.AddMxCell(mxCellOne);
+drawmateContext.SaveDiagram("/home/aaron/Documents/drawmate.drawio.xml");
